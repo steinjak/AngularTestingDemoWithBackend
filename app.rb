7fe1cf712079a2cfe6dab31996d8911b2aed2cd7
Stein Jakob Nordbø <steinjak@gmail.com>
2012-09-11 22:10:56 +0200

    Use mongodb for persistence

--- app.rb
+++ app.rb
@@ -1,14 +1,18 @@
 require 'sinatra'
+require 'mongo'
 
-categories = { :food => {:name => "Food", :blurb => "Yummy things"},
-    :sports => {:name => "Sports", :blurb => "Sporty things"},
-    :animals => {:name => "Animals", :blurb => "Living things"}
-}
+helpers do
+  def categories
+    set :db, "angular-shop-backend" unless settings.db?
+    db = Mongo::Connection.new.db(settings.db)
+    db.collection("categories")
+  end
+end
 
 get "/categories/:name" do
-  categories[params[:name].to_sym].to_json
+  categories.find_one(:_id => params[:name]).to_json
 end
 
 get "/categories" do
-  categories.to_json
+  categories.find().to_a.to_json
 end
--- tests/categories.rb
+++ tests/categories.rb
@@ -2,8 +2,10 @@ require './app.rb'
 require 'rspec'
 require 'rack/test'
 require 'json'
+require 'mongo'
 
 set :environment, :test
+set :db, "categories-tests"
 
 describe 'The browsing service' do
   include Rack::Test::Methods
@@ -12,12 +14,26 @@ describe 'The browsing service' do
     Sinatra::Application
   end
 
+  before(:each) do
+    test_data = [{:_id => "food", :name => "Food", :blurb => "Yummy things"},
+            {:_id => "sports", :name => "Sports", :blurb => "Sporty things"},
+            {:_id => "animals", :name => "Animals", :blurb => "Living things"}]
+
+    connection = Mongo::Connection.new
+    db = connection.db("categories-tests")
+    @cats = db.collection("categories")
+    test_data.each {|c| @cats.insert(c)}
+  end
+
+  after(:each) do
+    @cats.drop
+  end
+
   it "returns the top-level list of categories" do
     get "/categories"
     last_response.should be_ok
     list = JSON.parse(last_response.body)
-    list.length.should eq(3)
-    list.map {|c| c[1]["name"]}.sort().should eq(["Animals", "Food", "Sports"])
+    list.map {|c| c["name"]}.sort().should eq(["Animals", "Food", "Sports"])
   end
 
   it "returns categories by name" do
